6af1ee9e898ad09d7ce588780a131c6fb89b3d4a
hamdyaea <hamdy.aea@protonmail.com>
2024-11-17 10:17:16 +0100

    add the true command

---
+++ true.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+Name: Hamdy Abou El Anein
+Email: hamdy.aea@protonmail.com
+Date of creation:  17-11-2024
+Last update: 17-11-2024
+Version: 1.0
+Description: The true command from GNU coreutils in Python3.  
+Example of use: python3 true.py
+# echo $?
+
+'''
+import sys
+
+def main():
+    # Le programme ne fait rien mais renvoie un code de sortie de succès
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
+
